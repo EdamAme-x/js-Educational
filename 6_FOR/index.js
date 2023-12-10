09d25be04d340dd09b63702d3879e6c10625b9e2
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-10 07:51:16 +0000

    add

---
+++ 6_FOR/index.js
@@ -0,0 +1,12 @@
+const max = 10;
+
+for (let i = 0;i < max;i++) {
+    console.log(i)
+}
+
+// 以下の意味は次の次の次の章で分かる
+const list = ["a", "b", "c"];
+
+for (let i = 0;i < list.length;i++) {
+    console.log(list[i])
+}
---
+++ X_ASOBI_kabo/index.js
@@ -0,0 +1,11 @@
+// iine asonndete
+
+const test1 = 25565 !== 19132
+const test2 = "Unko"
+const test3 = "Tinko"
+if (test2 !== test3) {
+    console.log("MANKO!!")
+}
+// やらなきゃいけないことできたごめん落ちる
+// okばい 保存しとくまた明日
+// レッスンの続き書いとくわok
